98c28cbe103d6897330ba43b7654945e7f8a0e45
Jeniffer Oliveira <jeniffer.campos.241201@gmail.com>
2023-03-23 12:11:29 -0300

    corrigindo temperatura

--- temperatura.hs
+++ temperatura.hs
@@ -1,4 +1,4 @@
--- 3.2)	Faça	o	tipo		Temperatura		que	pode	ter	valores		Celsius	, Farenheit		ou		Kelvin	.	Implemente	as	funções: converterCelsius	:	recebe	um	valor		double		e	uma temperatura,	e	faz	a	conversão	para	Celsius. converterKelvin	:	recebe	um	valor		double		e	uma temperatura,	e	faz	a	conversão	para	Kelvin. converterFarenheit	:	 recebe	 um	 valor	 	double		 e uma	temperatura,	e	faz	a	conversão	para	Farenheit.
+
 
 data Temperatura = Celsius Double | Farenheit Double| Kelvin Double deriving Show 
 
@@ -13,6 +13,6 @@ converterFarenheit x (Farenheit f) = f
 converterFarenheit x (Kelvin k) = (k - 32) * (5 / 9) + 273
  
 converterKelvin :: Double -> Temperatura -> Double
-converterKelvin x (Celsius c) = c - 273
-converterKelvin x (Farenheit f) = (f - 273) * 1.8 + 32
+converterKelvin x (Celsius c) = c + 273
+converterKelvin x (Farenheit f) = (f + 273) * 1.8 + 32
 converterKelvin x (Kelvin k) = k
